dbe8930293b5cc987802038040f8b2f3c6d4a0b8
hp0119 <2205883@uad.ac.uk>
2022-09-22 12:43:26 +0100

    First commit - from command line

---
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProGitForProgrammers
+{
+    class ProGitForProgrammers
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there");
+            Console.ReadKey();
+        }
+
+        public int Tasks(int num1, int num2)
+        {
+            int num3 = 0;
+
+            // start tasks
+
+            return num3;
+        }
+    }
+}
