47a5ddca7da70c811ecf1f86bf93e0e065ad24d7
hp0119 <2205883@uad.ac.uk>
2022-09-29 11:54:43 +0100

    Adding function implement

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -11,13 +11,19 @@ namespace ProGitForProgrammers
         {
             Console.WriteLine("Hello there");
             Console.ReadKey();
+            int num1, num2, num3;
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            num3 = Tasks(num1, num2);
+            Console.WriteLine(num3);
         }
 
-        public int Tasks(int num1, int num2)
+        public static int Tasks(int num1, int num2)
         {
             int num3 = 0;
 
             // start tasks
+            num3 = num1 + num2;
 
             return num3;
         }
